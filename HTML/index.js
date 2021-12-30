933e6b046fce849488b3c89b3a60ad192bb67f86
kamiyakoh <nanayou791@gmail.com>
2021-12-30 17:51:50 +0900

    create footer

--- HTML/index.js
+++ HTML/index.js
@@ -13,3 +13,11 @@ fetch("header.html")
     .then(data => {
         document.querySelector("header").innerHTML = data;
     });
+
+fetch("footer.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
